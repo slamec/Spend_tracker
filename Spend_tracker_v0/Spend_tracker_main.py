03c4ac41d899ea5073b16cf97af7a824de08bebb
Miro <kopecky.mir@gmail.com>
2024-10-19 21:35:32 +0200

    added SQLite reference

--- Spend_tracker_v0/Spend_tracker_main.py
+++ Spend_tracker_v0/Spend_tracker_main.py
@@ -2,13 +2,25 @@
 #def add all the spends and categorize them
 #visualise the spends vs. monthly budget
 
-import pickle
+import sqlite3
+import os
 import datetime
 import currencies
 
-spends = [1, 2, 600, 900, 300]
+cwd = os.path.dirname(__file__)
+db_path = os.path.join(cwd, 'example.db')
+
+# Connect to the database
+conn = sqlite3.connect(db_path)
+c = conn.cursor()
+
+available_budget = []
+spend = []
 category = ['Housing', 'Car', 'Gasoline', 'Food']
-date_time = datetime.datetime.now()
+date_time_now = datetime.datetime.now()
+date_time_user = []
 currency = currencies.Currency.get_currency_formats()
+currency_user = []
 
-
+print(cwd)
+     
