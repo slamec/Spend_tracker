fb4533cddf04bc33e26e98c2348a423e09da9b38
Miro <kopecky.mir@gmail.com>
2024-10-19 20:47:14 +0200

    imports and variables added

--- Spend_tracker_v0/Spend_tracker_main.py
+++ Spend_tracker_v0/Spend_tracker_main.py
@@ -1,3 +1,14 @@
 #imports 
 #def add all the spends and categorize them
 #visualise the spends vs. monthly budget
+
+import pickle
+import datetime
+import currencies
+
+spends = [1, 2, 600, 900, 300]
+category = ['Housing', 'Car', 'Gasoline', 'Food']
+date_time = datetime.datetime.now()
+currency = currencies.Currency.get_currency_formats()
+
+
